23bce60a30b13fc598abfa30277e0f805b1e42cf
MogesBekele <mogesbekele32@gmail.com>
2025-11-19 18:29:49 +0300

    patch delete and quire method in nestjs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   /*
   GET /users
+  //quire paramemter /users?role = value
   GET /users/:id
   POST /users
   PATCH /users/:id
@@ -11,7 +21,7 @@ export class UsersController {
   */
 
   @Get()
-  findAll() {
+  findAll(@Query('role') role?: 'INTERN' | 'ADMIN' | 'ENGINEER') {
     return [];
   }
 
@@ -23,5 +33,14 @@ export class UsersController {
   @Post()
   create(@Body() user: {}) {
     return user;
+  }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpadte: {}) {
+    return { id, ...userUpadte };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return { id };
   }
 }
