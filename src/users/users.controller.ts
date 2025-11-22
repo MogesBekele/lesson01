3103e6ad403b2b236ce8e469655e3e83302f4d36
Mogesbek <mogesbekele32@gmail.com>
2025-11-22 21:31:27 +0300

    add unary plus to change the string to number

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -29,7 +29,7 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.UsersService.findOne(id);
+    return this.UsersService.findOne(+id);
   }
 
   @Post()
